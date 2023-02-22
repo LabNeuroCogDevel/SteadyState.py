bf3316171673351cdcf998778e9df48456cd31cd
Will Foran <willforan@gmail.com>
2023-02-22 10:28:25 -0500

    🎨 steadystate.py: blacken

--- steadystate.py
+++ steadystate.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-from psychopy import visual, core,  event, logging, prefs
+from psychopy import visual, core, event, logging, prefs
 from random import shuffle
 import psychtoolbox as ptb
-prefs.hardware['audioLib'] = ['PTB', 'pyo','pygame']
+
+prefs.hardware["audioLib"] = ["PTB", "pyo", "pygame"]
 from psychopy.sound import Sound
 
 
@@ -16,9 +17,10 @@ Using functions cribbed from MGSEncMem:
 https://github.com/LabNeuroCogDevel/mgs_encode_memory.py
 """
 
-    
+
 def pt(msg):
     print("[%.02f] %s" % (core.getTime(), msg))
+
 
 def wait_until(stoptime, maxwait=30):
     """
@@ -28,14 +30,17 @@ def wait_until(stoptime, maxwait=30):
     so we dont get stuck. defaults to 30 seconds
     """
     if stoptime - core.getTime() > maxwait:
-        raise ValueError("request to wait until stoptime is more than " +
-                         "30 seconds, secify maxwait to avoid this error")
+        raise ValueError(
+            "request to wait until stoptime is more than "
+            + "30 seconds, secify maxwait to avoid this error"
+        )
     # will hog cpu -- no pyglet.media.dispatch_events here
     while core.getTime() < stoptime:
         continue
 
+
 def create_window(fullscr, screen=0):
-    """ create window either fullscreen or 800,600
+    """create window either fullscreen or 800,600
     hide mouse cursor and make active
     """
     # setup screen
@@ -52,7 +57,8 @@ def create_window(fullscr, screen=0):
     # flip twice to get the color
     win.flip()
     win.flip()
-    return(win)
+    return win
+
 
 class SteadyState:
     def __init__(self):
@@ -61,35 +67,34 @@ class SteadyState:
         self.zeroTTL = True
         self.verbose = False
         self.fullscreen = True
-        #self.pp_address=0xDFF8
+        # self.pp_address=0xDFF8
         # 2023-02-20
-        self.pp_address=0xD010
+        self.pp_address = 0xD010
 
-        # load up 
+        # load up
         self.init_pp()
-        self.sound = {x : Sound('click-%sHzTrain.wav' %x )
-          for x in ['20','30','40']}
+        self.sound = {x: Sound("click-%sHzTrain.wav" % x) for x in ["20", "30", "40"]}
 
         self.win = create_window(self.fullscreen)
         # a fixation cross with 2x normal size
-        self.cross = visual.TextStim(self.win, text='+', name='iti_fixation',
-                                     color='white', bold=True)
+        self.cross = visual.TextStim(
+            self.win, text="+", name="iti_fixation", color="white", bold=True
+        )
         self.cross.size = 2
-        
+
         # flip and draw fixation cross
         self.cross.draw()
         self.win.flip()
-        
-
 
     def init_pp(self):
         "initialize parallel port if we want it"
         if not self.usePP:
             return
         # initialize parallel port
-        if not hasattr(self, 'port'):
+        if not hasattr(self, "port"):
             # might need to 'pip install pyparallel'
             from psychopy import parallel
+
             self.port = parallel.ParallelPort(address=self.pp_address)
 
     def send_ttl(self, thistrigger):
@@ -105,7 +110,7 @@ class SteadyState:
         if self.verbose:
             print("eeg code %s" % thistrigger)
         if self.zeroTTL:
-            core.wait(.01)  # wait 10ms and send zero
+            core.wait(0.01)  # wait 10ms and send zero
             self.port.setData(0)
 
     def play_snd(self, snd, when):
@@ -114,20 +119,21 @@ class SteadyState:
 
     def instructions(self):
         # todo. text is too large?
-        self.cross.text="SteadyState. Ready?"
+        self.cross.text = "SteadyState. Ready?"
         self.cross.draw()
         self.win.flip()
         event.waitKeys()
-        
+
         # same screen for duration of task
-        self.cross.text="" # not "+". but just empty black screen
+        self.cross.text = ""  # not "+". but just empty black screen
         self.cross.draw()
         self.win.flip()
+
 
 if __name__ == "__main__":
     n_each = 150
     dur = 1.1
-    order = ['20','30','40']
+    order = ["20", "30", "40"]
 
     # 20221207 - is totally random okay?
     shuffle(order)
@@ -139,23 +145,22 @@ if __name__ == "__main__":
 
     start = core.getTime()
     first_wait = 1.5  # seconds
-    times=[start + first_wait + i*dur
-           for i in range(n_each*len(order))]
+    times = [start + first_wait + i * dur for i in range(n_each * len(order))]
 
     pt("start")
     t.send_ttl(128)
 
     # 3 different train frequencies
-    for sndi,snd in enumerate(order):
-        ttl = int(snd)/10;
+    for sndi, snd in enumerate(order):
+        ttl = int(snd) / 10
 
         # 150 of each
         for i in range(n_each):
-            ii = sndi*n_each + i
+            ii = sndi * n_each + i
             stime = times[ii]
 
             t.play_snd(snd, stime)
-            wait_until(stime-.001)
+            wait_until(stime - 0.001)
             t.send_ttl(ttl)
 
             pt("%d: %s %d (ideal %.02f)" % (ii, snd, i, stime))
